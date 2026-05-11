1e6e3790594914c2267e4c0eb927a742574788b6
Ana.S Rosero <1081055280@u.icesi.edu.co>
2026-05-11 17:40:28 -0500

    Add: assets and libraries

---
+++ Same-Beat/src/data/music/songs.ts
@@ -0,0 +1,12 @@
+import dardosSong from "../../assets/dardos.mp3";
+import coverDardos from "../../assets/cover.jpg";
+
+export const songs = [
+  {
+    id: 1,
+    name: "Romeo",
+    artist: "Prince Royce",
+    image: coverDardos,
+    audio: dardosSong
+  }
+];
